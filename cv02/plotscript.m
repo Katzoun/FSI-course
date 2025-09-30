1e3782fbc9b097f19c80e9fa83c1aea785a8bd2c
Tomas <tomas.janousek02@gmail.com>
2025-09-30 10:41:36 +0200

    ex2

---
+++ cv02/plotscript.m
@@ -0,0 +1,13 @@
+time = 0:0.1:4;
+t = out.ScopeData.time;
+x_ode4= out.ScopeData.signals.values;
+close all
+
+plot(t,x, 'LineWidth',2)
+hold on
+grid on
+plot(t,x_ode4, 'LineWidth',2)
+plot(time,exp(-time), "LineWidth",2, "LineStyle","--")
+legend('Ode1','Ode4', 'Analyticky')
+ylabel('x')
+xlabel('t [s]')
